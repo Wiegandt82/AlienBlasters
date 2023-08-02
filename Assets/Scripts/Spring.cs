46634e2c7a90e51de977d933f4e655f4d5bdbb81
Wiegandt82 <wiegandt@hotmail.com>
2023-08-02 13:51:13 +0200

    Added sound effects to spring

--- Assets/Scripts/Spring.cs
+++ Assets/Scripts/Spring.cs
@@ -7,17 +7,23 @@ public class Spring : MonoBehaviour
     [SerializeField] Sprite _sprung;
     SpriteRenderer _spriteRenderer;
     Sprite _defaultSprite;
+    AudioSource _audioSource;
 
     void Awake()
     {
         _spriteRenderer = GetComponent<SpriteRenderer>();
         _defaultSprite = _spriteRenderer.sprite;
+        _audioSource = GetComponent<AudioSource>();
     }
 
     void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.collider.CompareTag("Player"))
+        {
             _spriteRenderer.sprite = _sprung;
+            _audioSource.Play();
+        }
+        
     }
 
     void OnCollisionExit2D(Collision2D collision)
