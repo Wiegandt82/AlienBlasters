2d4f0651ec194d2b7e885d8c25670c27a120adb3
Wiegandt82 <wiegandt@hotmail.com>
2023-08-03 16:00:10 +0200

    Added the jumping frog prefab with variable jump count and distance

--- Assets/Scripts/Frog.cs
+++ Assets/Scripts/Frog.cs
@@ -7,28 +7,38 @@ public class Frog : MonoBehaviour
 {
     [SerializeField] float _jumpDelay = 3;
     [SerializeField] Vector2 _jumpForce;
-    [SerializeField] Sprite _jumpSprite; 
+    [SerializeField] Sprite _jumpSprite;
+    [SerializeField] int _jumps = 2;
 
     Rigidbody2D _rb;
     SpriteRenderer _spriteRenderer;
     Sprite _defaultSprite;
+    int _jumpsRemaining;
+    
+
 
     void Awake()
     {
         _rb = GetComponent<Rigidbody2D>();
         _spriteRenderer = GetComponent<SpriteRenderer>();
         _defaultSprite = _spriteRenderer.sprite;
+        _jumpsRemaining = _jumps;
 
         InvokeRepeating("Jump", _jumpDelay, _jumpDelay);
     }
 
     void Jump()
     {
+        if(_jumpsRemaining <= 0)
+        {
+            _jumpForce *= new Vector2(-1, 1);
+            _jumpsRemaining = _jumps;
+        }
+        _jumpsRemaining--;
+
         _rb.AddForce(_jumpForce);
-        _jumpForce *= new Vector2(-1, 1);
-        _spriteRenderer.flipX = !_spriteRenderer.flipX;
+        _spriteRenderer.flipX = _jumpForce.x > 0;
         _spriteRenderer.sprite = _jumpSprite;
-       
     }
 
     void OnCollisionEnter2D(Collision2D collision)
