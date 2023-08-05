22d4974e88866f088627ad34a582d2d19f26fc71
Wiegandt82 <wiegandt@hotmail.com>
2023-08-05 09:09:38 +0200

    Menu works with LoadLevelButtons

---
+++ Assets/Scripts/LoadLevelButton.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadLevelButton : MonoBehaviour
+{
+    [SerializeField] string _levelName;
+    public void Load()
+    {
+        SceneManager.LoadScene(_levelName);
+    }
+}
