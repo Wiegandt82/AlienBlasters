e4ddc208845ff7ec934551996aef01512558668f
Krzysztof Wiegandt <wiegandt@hotmail.com>
2023-08-10 11:07:16 +0200

    Added delete confirmation to saved games

--- Assets/Scripts/LoadGameButton.cs
+++ Assets/Scripts/LoadGameButton.cs
@@ -11,7 +11,11 @@ public class LoadGameButton : MonoBehaviour
 
     public void LoadGame() => GameManager.Instance.LoadGame(_gameName);
 
-    public void DeleteGame() => GameManager.Instance.DeleteGame(_gameName);
+    public void DeleteGame()
+    {
+        GameManager.Instance.DeleteGame(_gameName);
+        Destroy(gameObject);
+    }
 
     public void SetGameName(string gameName)
     {
