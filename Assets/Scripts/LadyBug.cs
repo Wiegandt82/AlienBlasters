25dd9b599f2dafa7ecc7f4274de7eff8f15b5d01
Krzysztof Wiegandt <wiegandt@hotmail.com>
2023-08-12 21:53:54 +0200

    Added mechanic to LadyBug to change direction when hitting a wall

--- Assets/Scripts/LadyBug.cs
+++ Assets/Scripts/LadyBug.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class Ladybug : MonoBehaviour
 {
     [SerializeField] float _speed = 1f;
-    Vector2 _direction = Vector2.left;
+    [SerializeField] float _raycastDistance;
 
+    Vector2 _direction = Vector2.left;
     SpriteRenderer _spriteRenderer;
     Collider2D _collider;
     Rigidbody2D _rigidbody;
+    
 
     void Awake()
     {
@@ -21,6 +23,21 @@ public class Ladybug : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Vector2 offset = _direction * _collider.bounds.extents.x;
+        Vector2 origin = (Vector2)transform.position + offset;
+
+        var hits = Physics2D.RaycastAll(origin, _direction, _raycastDistance);
+
+        foreach (var hit in hits)
+        {
+            if (hit.collider != null && hit.collider.gameObject != gameObject)
+            {
+                _direction *= -1;
+                _spriteRenderer.flipX = _direction == Vector2.right;
+                break;
+            }
+        }
+
         _rigidbody.velocity = new Vector2(_direction.x * _speed, _rigidbody.velocity.y);
     }
 }
