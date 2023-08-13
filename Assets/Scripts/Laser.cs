a04c9661f509a7b459f663da7b05fe32c50254f3
Krzysztof Wiegandt <wiegandt@hotmail.com>
2023-08-13 16:24:19 +0200

    Added laser burst to the laser beam

--- Assets/Scripts/Laser.cs
+++ Assets/Scripts/Laser.cs
@@ -6,6 +6,7 @@ public class Laser : MonoBehaviour
 {
     [SerializeField] Vector2 _direction = Vector2.left; //(Shooting) Vector for direction of laser
     [SerializeField] float _distance = 10f;             //(Shooting) float for distance of laser
+    [SerializeField] SpriteRenderer _laserBurst; 
 
     LineRenderer _lineRenderer;
     bool _isOn;
@@ -25,23 +26,33 @@ public class Laser : MonoBehaviour
     void Update()
     {
         if (_isOn == false)
+        {
+            _laserBurst.enabled = false;                                                //turns off laser burst when laser is not on
             return;
+        }
+            
 
         //(Shooting) End of LineRenderer = current position Vector2(transform.position) + offset (_direction * _distance)
         var endPoint = (Vector2)transform.position + (_direction * _distance);
 
-        var firstThing = Physics2D.Raycast(transform.position, _direction, _distance); //(Shooting) Will return first collider it hits
+        var firstThing = Physics2D.Raycast(transform.position, _direction, _distance);      //(Shooting) Will return first collider it hits
 
-        if (firstThing.collider) //if raycast hits something
+        if (firstThing.collider)                                                            //if raycast hits something
         {
-            endPoint = firstThing.point; //updates endPoint with point where raycast hit
+            endPoint = firstThing.point;                                                    //updates endPoint with point where raycast hit
+            _laserBurst.transform.position = endPoint;                                      //this set laserBurst sprite on end of laser
+            _laserBurst.enabled = true;                                                     //turns laserBurst when laser hits something
+            _laserBurst.transform.localScale = Vector3.one * (0.5f + Mathf.PingPong(Time.time, 0.25f)); //This will make burst pulsing
 
-            //take LadyBug from hit of raycast
-            var laserDamageable = firstThing.collider.GetComponent<ITakeLaserDamage>();
-            //if Ladybug exists take damage
-            if (laserDamageable != null)
+            var laserDamageable = firstThing.collider.GetComponent<ITakeLaserDamage>();     //take LadyBug from hit of raycast
+
+            if (laserDamageable != null)                                                    //if Ladybug exists take damage
                 laserDamageable.TakeLaserDamage();
         }
+        else
+            _laserBurst.enabled = false;
+
+
         //(shooting) Sets position of LineRenderer
         _lineRenderer.SetPosition(1, endPoint);                                 
     }
